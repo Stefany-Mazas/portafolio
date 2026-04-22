42e5525ce972aae050eb5beffac7a16b3bbfe360
stefany mazas <stefanymazas@gmail.com>
2026-04-21 23:37:52 -0600

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ThreeSceneComponent } from './components/three-scene.component';
-
-export const routes: Routes = [
-  { path: '', component: ThreeSceneComponent },
-  { path: '**', redirectTo: '' }
-];
